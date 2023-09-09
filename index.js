4c27a4c48f6bb860723c815ae7ab1acab9d1f168
ISHA SINGH <92418692+isha8455@users.noreply.github.com>
2023-09-09 17:20:42 +0000

    intojs

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const state = {
+    taskList: [],
+};
+
+// DOM 
+const taskContents = document.querySelector(".task__contents")
+const taskModal = document.querySelector(".task__modal__body");
+
+const htmlTaskContent = ({id, title, description, type, url}) => {
+
+
+};
